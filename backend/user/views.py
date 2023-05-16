4d01d7331759e8d4d1294713034fdbecb07dce49
JQKonatsu <zwj1813@163.com>
2023-05-16 14:50:36 +0800

    fin register view

--- backend/user/views.py
+++ backend/user/views.py
@@ -8,32 +8,40 @@ from user.models import User, Passenger
 
 @api_view(['POST'])
 def register(request):
-    data = request.data
-    message = '请检查填写内容'
-    username = data['username']
+    try:
+        data = request.data
+        username = data['username']
+        password = data['password']
+        confirmed_password = data['confirmed_password']
 
-    same_name_user = User.objects.filter(username=username)
-    if same_name_user:
-        message = '用户名已存在'
-        return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+        if password != confirmed_password:
+            message = '两次输入的密码不同'
+            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
 
-    user = User()
-    user.username = username
-    user.password = make_password(data['password'])
-    if 'email' in data and data['email']:
-        user.email = data['email']
-    user.save()
+        same_name_user = User.objects.filter(username=username)
+        if same_name_user:
+            message = '用户名已存在'
+            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
 
-    passenger = Passenger(
-        id_type=data['id_type'],
-        name=data['name'],
-        id_number=data['id_number'],
-        ticket_type=data['ticket_type'],
-        phone_region=data['phone_region'],
-        phone_number=data['phone_number'],
-    )
-    passenger.user = user
-    passenger.save()
-    message = '用户注册成功'
+        user = User()
+        user.username = username
+        user.password = make_password(password)
+        if 'email' in data and data['email']:
+            user.email = data['email']
 
-    return Response({'message': message}, status=status.HTTP_201_CREATED)
+        passenger = Passenger(
+            id_type=data['id_type'],
+            name=data['name'],
+            id_number=data['id_number'],
+            ticket_type=data['ticket_type'],
+            phone_region=data['phone_region'],
+            phone_number=data['phone_number'],
+        )
+        passenger.user = user
+        user.save()
+        passenger.save()
+        message = '用户注册成功'
+        return Response({'message': message}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        message = '发生错误：{}'.format(str(e))
+        return Response({'message': message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
