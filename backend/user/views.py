5a4122146f9a2e41af0df2943cbf91a1a565870b
JQKonatsu <zwj1813@163.com>
2023-05-18 12:11:19 +0800

    fin login and logout

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from rest_framework.response import Response
 
 from user.models import User, Passenger
@@ -12,11 +12,6 @@ def register(request):
         data = request.data
         username = data['username']
         password = data['password']
-        confirmed_password = data['confirmed_password']
-
-        if password != confirmed_password:
-            message = '两次输入的密码不同'
-            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
 
         same_name_user = User.objects.filter(username=username)
         if same_name_user:
@@ -42,6 +37,48 @@ def register(request):
         passenger.save()
         message = '用户注册成功'
         return Response({'message': message}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        message = '发生错误：{}'.format(str(e))
+        return Response({'message': message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view['POST']
+def login(request):
+    try:
+        if request.session.get('is_login', None):
+            message = '不允许重复登录'
+            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except Exception as e:
+            message = '用户不存在'
+            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+        if check_password(password, user.password):
+            request.session['is_login'] = True
+            request.session['user_id'] = user.id
+            request.session['username'] = user.username
+            message = '登录成功'
+            return Response({'message': message}, status=status.HTTP_200_OK)
+        else:
+            message = '用户名或密码错误'
+            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        message = '发生错误：{}'.format(str(e))
+        return Response({'message': message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view['GET']
+def logout(request):
+    try:
+        if not request.session.get('is_login', None):
+            message = '请先登录'
+            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+        request.session.flush()
+        message = '登出成功'
+        return Response({'message': message}, status=status.HTTP_200_OK)
     except Exception as e:
         message = '发生错误：{}'.format(str(e))
         return Response({'message': message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
