fef7da25df90d47b6c87b1abd5a45c7d33078e67
JQKonatsu <zwj1813@163.com>
2023-05-19 17:04:46 +0800

    change models in train

--- backend/train/models.py
+++ backend/train/models.py
@@ -7,12 +7,7 @@ from user.models import User
 class Station(models.Model):
     name = models.CharField(max_length=5, unique=True, verbose_name="车站名", null=False)
     code = models.CharField(max_length=3, unique=True, verbose_name="车站代码", null=False)
-    station_num = models.PositiveSmallIntegerField(unique=True, verbose_name="车站编号")
-    city_num = models.PositiveSmallIntegerField(verbose_name="城市编号")
-    city_name = models.CharField(max_length=10, verbose_name="城市名", null=False)
-
-    def __str__(self):
-        return '<id=%s> %s' % (self.id, self.name)
+    city = models.CharField(max_length=10, verbose_name="城市名", null=False)
 
     class Meta:
         verbose_name = '车站'
@@ -29,46 +24,12 @@ class Train(models.Model):
     name = models.CharField(max_length=128, unique=True, verbose_name="车次", null=False)
     train_type = models.CharField(max_length=8, choices=train_type_choices, default="HSR", verbose_name="车次类型",
                                   null=False)
-
-    def __str__(self):
-        return '<id=%s> %s' % (self.id, self.name)
+    departure_time = models.DateTimeField(verbose_name='出发日期')
 
     class Meta:
         verbose_name = '列车'
         verbose_name_plural = '列车'
         ordering = ['-name']
-
-
-class Seat(models.Model):
-    seat_type_choices = (
-        ('business', '商务座'),
-        ('first', '一等座'),
-        ('second', '二等座'),
-        ('hardSleeper', '硬卧'),
-        ('softSleeper', '软卧'),
-        ('noSeat', '无座'),
-        ('hardSeat', '硬座'),
-    )
-
-    train = models.ForeignKey(Train, on_delete=models.CASCADE, verbose_name='车次', null=False)
-    carriage = models.PositiveSmallIntegerField(verbose_name='车厢号', null=False)
-    seat_type = models.CharField(max_length=16, choices=seat_type_choices, default='second', verbose_name='座位类型',
-                                 null=False)
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=Decimal('0.00'), verbose_name='价格')
-    location = models.CharField(max_length=4, verbose_name='位置', null=False)
-    is_available = models.BooleanField(verbose_name='是否可用')
-
-    def __str__(self):
-        return '<id=%s> %s %s' % (self.id, self.train_id, self.location)
-
-    class Meta:
-        verbose_name = '座位'
-        verbose_name_plural = '座位'
-        ordering = ['-id']
-        constraints = [
-            models.UniqueConstraint(fields=['train_id', 'carriage', 'location'],
-                                    name='unique_train_id_carriage_location')
-        ]
 
 
 class Stop(models.Model):
@@ -79,20 +40,52 @@ class Stop(models.Model):
     # 出发时间根据上面两项计算，以处理跨天的情况
     sequence = models.IntegerField(verbose_name='序号', null=False)
 
-    def __str__(self):
-        return '<id=%s> train=%s station=%s %s %s' % (
-            self.id, self.train_id, self.station_id, self.arrival_time, self.departureTime)
-
     class Meta:
         verbose_name = '途径站'
         verbose_name_plural = '途径站'
         ordering = ['-id']
 
 
+class Carriage(models.Model):
+    TYPE_CHOICES = (
+        ('BUS', '商务舱'),
+        ('FST', '一等舱'),
+        ('SND', '二等舱'),
+        ('SOF', '软卧'),
+        ('HAW', '硬卧'),
+        ('HAZ', '硬座'),
+    )
+
+    carriage_num = models.PositiveSmallIntegerField(verbose_name='车厢号')
+    type = models.CharField(max_length=3, choices=TYPE_CHOICES, verbose_name='车厢类型')
+    total_num = models.PositiveSmallIntegerField(verbose_name='座位总数')
+    total_available = models.PositiveSmallIntegerField(verbose_name='剩余座位数')
+    seats = models.JSONField(default=dict, verbose_name='座位分布')
+    train = models.ForeignKey(Train, on_delete=models.CASCADE, verbose_name='所属车次')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=Decimal('0.00'), verbose_name='价格')
+
+    class Meta:
+        verbose_name = '车厢'
+        verbose_name_plural = '车厢'
+        ordering = ['-id']
+
+    def save(self, *args, **kwargs):
+        if not self.seats:
+            self.seats = {
+                'A': 0,
+                'B': 0,
+                'C': 0,
+                'D': 0,
+                'F': 0,
+            }
+        super().save(*args, **kwargs)
+
+
 class Order(models.Model):
     user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE, null=False)
     train = models.ForeignKey(Train, verbose_name='车次', on_delete=models.CASCADE, null=False)
-    seat = models.ForeignKey(Seat, verbose_name='座位', on_delete=models.CASCADE, null=False)
+    carriage_num = models.PositiveSmallIntegerField(verbose_name='车厢号')
+    seat_num = models.CharField(max_length=3, verbose_name='座位位置')
     departure_station = models.ForeignKey(Stop, verbose_name='出发站', related_name='arrivals',
                                           on_delete=models.CASCADE)
     departure_time = models.DateTimeField(verbose_name='出发时间')
@@ -100,10 +93,6 @@ class Order(models.Model):
                                         on_delete=models.CASCADE)
     arrival_time = models.DateTimeField(verbose_name='到达时间')
     create_time = models.DateTimeField(verbose_name='创建时间', null=False)
-
-    def __str__(self):
-        return '<id=%s> train=%s station=%s seat=%s %s %s' % (
-            self.id, self.train_id, self.station_id, self.seat_id, self.departure_station, self.arrival_station)
 
     class Meta:
         verbose_name = '订单'
--- backend/train/views.py
+++ backend/train/views.py
@@ -1,1 +1,45 @@
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
+from .models import Station
+
+
+@api_view(['POST'])
+def add_station(request):
+    try:
+        # 检查用户身份
+        identity = request.session.get('identity', None)
+        if identity != 'system_admin':
+            message = '无效权限'
+            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 获取请求体中的数据
+        data = request.data
+        name = data['name']
+        code = data['code']
+        city = data['city']
+
+        # 创建车站
+        station = Station(name=name, code=code, city=city)
+        station.save()
+
+        message = '车站添加成功'
+        return Response({'message': message}, status=status.HTTP_201_CREATED)
+
+    except Exception as e:
+        message = '发生错误：{}'.format(str(e))
+        return Response({'message': message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['POST'])
+def add_train(request):
+    try:
+        identity = request.session.get('identity', None)
+        if identity != 'system_admin':
+            message = '无效权限'
+            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+
+    except Exception as e:
+        message = '发生错误：{}'.format(str(e))
+        return Response({'message': message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
