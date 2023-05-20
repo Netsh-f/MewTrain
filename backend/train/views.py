37cca038342e4109429564ae10e78856ff2f9810
JQKonatsu <zwj1813@163.com>
2023-05-20 15:29:49 +0800

    add add_train view

--- backend/train/views.py
+++ backend/train/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 
-from .models import Station
+from .models import Station, Train, Carriage, Stop
 
 
 @api_view(['POST'])
@@ -39,7 +39,51 @@ def add_train(request):
         if identity != 'system_admin':
             message = '无效权限'
             return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+        data = request.data
+        train_name = data.get("train_name")
+        departure_time = data.get("departure_time")
+        train_type = data.get("train_type")
+        carriages = data.get("carriages")
+        stops = data.get("stops")
 
+        train = Train.objects.create(
+            train_name=train_name,
+            departure_time=departure_time,
+            train_type=train_type
+        )
+
+        for carriage_data in carriages:
+            carriage_num = carriage_data.get("carriage_num")
+            total_num = carriage_data.get("total_num")
+            price = carriage_data.get("price")
+            seats = carriage_data.get("seats")
+
+            carriage = Carriage.objects.create(
+                carriage_num=carriage_num,
+                total_num=total_num,
+                price=price,
+                train=train,
+                seats=seats
+            )
+
+        for stop_data in stops:
+            station_name = stop_data.get("station_name")
+            arrival_time = stop_data.get("arrival_time")
+            duration = stop_data.get("duration")
+            sequence = stop_data.get("sequence")
+
+            # 获取对应的站点对象
+            station = Station.objects.get(name=station_name)
+            stop = Stop.objects.create(
+                station=station,
+                train=train,
+                arrival_time=arrival_time,
+                duration=duration,
+                sequence=sequence
+            )
+
+        message = '列车添加成功'
+        return Response({'message': message}, status=status.HTTP_201_CREATED)
     except Exception as e:
         message = '发生错误：{}'.format(str(e))
         return Response({'message': message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
