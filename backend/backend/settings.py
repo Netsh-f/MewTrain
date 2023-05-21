f8efe11497980a847cbd5798ef4d6b195f5f99be
JQKonatsu <zwj1813@163.com>
2023-05-21 13:16:49 +0800

    add CORS_ALLOW_METHODS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,3 +135,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+]
