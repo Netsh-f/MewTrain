6280f6bc596430771d1c1814e1389d736d38a29c
王晰 <21371192>
2023-05-24 17:01:00 +0800

    a commit for x

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,10 @@ const page2 = () => import('../components/page2.vue');
 const page3 = () => import('../components/page3.vue');
 const TicketInquiry = () => import('../components/TicketInquiry.vue')
 const TicketOrder = () => import('../components/TicketOrder.vue')
-
+const xxgl_home = () => import('../components/xxgl/xxgl_home.vue')
+const xxgl_check = () => import('../components/xxgl/xxgl_check.vue')
+const xxgl_message = () => import('../components/xxgl/xxgl_message.vue')
+const xxgl_setting = () => import('../components/xxgl/xxgl_setting.vue')
 
 const routes= [
     {
@@ -18,54 +21,76 @@ const routes= [
       component:Login,
     },
 
-    {path : '/',
-    name:'Home',
-    component:Home,
-    redirect: {name: "page1"},
-    children:[
-      {
-        path: "/TicketInquiry",
-        name: "TicketInquiry",
-        component: TicketInquiry,
-        meta: {
-          title: "车票查询"
-        }
-      },
-      {
-        path: "/TicketOrder",
-        name: "TicketOrder",
-        component: TicketOrder,
-        meta: {
-          title: "车票订购"
-        }
-      },
-     // 以下界面都是哪来测试跳转的
-      {
-        path: "/page1",
-        name: "page1",
-        component:page1,
-        meta:{
-            title:"测试界面1"
-        }
-      },
-      {
-        path: "/page2",
-        name: "page2",
-        component:page2,
-        meta:{
-            title:"测试界面2"
-        }
-      },
-      {
-        path: "/page3",
-        name: "page3",
-        component:page3,
-        meta:{
-            title:"测试界面3"
-        }
-      },
-    ],
-   },
+    {
+      path : '/',
+      name:'Home',
+      component:Home,
+      redirect: {name: "page1"},
+      children:[
+        {
+          path: "/TicketInquiry",
+          name: "TicketInquiry",
+          component: TicketInquiry,
+          meta: {
+            title: "车票查询"
+          }
+        },
+        {
+          path: "/TicketOrder",
+          name: "TicketOrder",
+          component: TicketOrder,
+          meta: {
+            title: "车票订购"
+          }
+        },
+      // 以下界面都是哪来测试跳转的
+        {
+          path: "/page1",
+          name: "page1",
+          component:page1,
+          meta:{
+              title:"测试界面1"
+          }
+        },
+        {
+          path: "/page2",
+          name: "page2",
+          component:page2,
+          meta:{
+              title:"测试界面2"
+          }
+        },
+        {
+          path: "/page3",
+          name: "page3",
+          component:page3,
+          meta:{
+              title:"测试界面3"
+          }
+        },
+      ],
+    },
+    //信息管理
+    {
+      path:'/xxgl1',
+      name: 'xxgl_home',
+      component:xxgl_home,
+    },
+    {
+      path: '/xxgl2',
+      name:'xxgl_message',
+      component:xxgl_message,
+    },
+    {
+      path: '/xxgl3',
+      name:'xxgl_check',
+      component:xxgl_check,
+    },
+    {
+      path: '/xxgl4',
+      name:'xxgl_setting',
+      component:xxgl_setting,
+    },
 
 ]
 const router = createRouter({
