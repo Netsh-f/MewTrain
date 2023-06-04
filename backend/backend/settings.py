d91e0543232635bd64151382df6c707bba62ca56
JQKonatsu <zwj1813@163.com>
2023-06-04 15:12:43 +0800

    remove rest

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,12 +87,7 @@ DATABASES = {
     }
 }
 
-# JWT
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
