3d8baa9347251c7d118d925413c4448eb6197f5d
JQKonatsu <zwj1813@163.com>
2023-06-06 22:45:08 +0800

    add api/

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("user/", include('user.urls')),
-    path("train/", include('train.urls')),
+    path("api/user/", include('user.urls')),
+    path("api/train/", include('train.urls')),
 ]
