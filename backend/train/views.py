1b940755a9da30e0b97959aae185e9f0807e3256
JQKonatsu <zwj1813@163.com>
2023-06-08 01:22:43 +0800

    update add_ticket

--- backend/train/views.py
+++ backend/train/views.py
@@ -137,7 +137,7 @@ def add_ticket(request):
 
         current_date = start_date
         while current_date <= end_date:
-            if carriages[0].ticket_set.all().first() is not None:
+            if carriages[0].ticket_set.all().filter(date=current_date.date()).first() is not None:
                 current_date += timedelta(days=1)
                 continue
             for carriage in carriages:
