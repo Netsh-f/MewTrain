e78d27cc509b41fab9e284bf34ab1227b08705af
尤世博 <21371413>
2023-06-08 14:02:11 +0800

    rubbish

--- src/components/WELCOME.vue
+++ src/components/WELCOME.vue
@@ -8,40 +8,41 @@
         </el-carousel-item>
       </el-carousel>
     </div>
-  </template>
+</template>
   
   
-  <script setup>
-  const imgwrap = [
-    { url: require('../assets/imgs/ChangSha1.jpg') },
-    { url: require('../assets/imgs/TheGreatWall1.jpg') },
-    { url: require('../assets/imgs/HongKong2.jpg') },
-    { url: require('../assets/imgs/HongKong1.jpg') },
-  ];
-  </script>
+<script setup>
+const imgwrap = [
+  { url: require('../assets/imgs/ChangSha1.jpg') },
+  { url: require('../assets/imgs/TheGreatWall1.jpg') },
+  { url: require('../assets/imgs/HongKong2.jpg') },
+  { url: require('../assets/imgs/HongKong1.jpg') },
+];
+</script>
   
-  <style scoped>
-  .hcontainer {
-    width: 100%;
-    height: 100%;
-  }
-  
-  .optimized-carousel {
-    height: 100%;
-    width: 100%;
-  }
-  
-  .carousel-image-wrapper {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  
-  .carousel-image {
-    max-width: 100%;
-    max-height: 100%;
-    object-fit: contain;
-  }
-  </style>
+<style scoped>
+.hcontainer {
+  width: 100%;
+  height: 100%;
+
+}
+
+.optimized-carousel {
+  height: 100%;
+  width: 100%;
+}
+
+.carousel-image-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.carousel-image {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+</style>
