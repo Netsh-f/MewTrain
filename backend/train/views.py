30c2d8b9415156a0154e5f65bd6a3afadc895494
JQKonatsu <zwj1813@163.com>
2023-06-08 10:13:05 +0800

    update rebook

--- backend/train/views.py
+++ backend/train/views.py
@@ -855,6 +855,11 @@ def rebook(request):
 
         order = response
 
+        logger.error(data)
+        logger.error(f'original_order.date:{type(original_order.date)}')
+        logger.error(f'original_order.start_stop.arrival_time:{type(original_order.start_stop.arrival_time)}')
+        logger.error(f'order.date.date:{type(order.date)}')
+        logger.error(f'order.start_stop.arrival_time:{type(order.start_stop.arrival_time)}')
         origin_time = datetime.combine(original_order.date, original_order.start_stop.arrival_time)
         time = datetime.combine(order.date, order.start_stop.arrival_time)
         if origin_time - time > timedelta(hours=24) or time - origin_time > timedelta(hours=24):
